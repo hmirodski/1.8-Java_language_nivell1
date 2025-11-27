7f4a30cc3e6a0ced5ec4edf18acc1008454a8205
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-27 13:19:07 +0100

    feat: created class FilterByLetterO

---
+++ src/main/java/exercise01/FilterByLetterO.java
@@ -0,0 +1,13 @@
+package exercise01;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterByLetterO {
+
+    public List<String> filter(List<String>list){
+        return list.stream()
+                .filter(w -> w.contains("o"))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/exercise01/Main.java
@@ -0,0 +1,10 @@
+package exercise01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
