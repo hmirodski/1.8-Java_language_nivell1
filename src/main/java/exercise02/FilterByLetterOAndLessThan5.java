134fc567dcc32333c0feb718ba6279751876af68
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-27 13:38:59 +0100

    feat created FilterByLetterOAndLessThan5

---
+++ src/main/java/exercise02/FilterByLetterOAndLessThan5.java
@@ -0,0 +1,14 @@
+package exercise02;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterByLetterOAndLessThan5 {
+    public List<String> filter(List<String> list) {
+        return list.stream()
+                .filter(w -> w.contains("o"))
+                .filter(x -> x.length() < 5)
+                .collect(Collectors.toList());
+
+    }
+}
