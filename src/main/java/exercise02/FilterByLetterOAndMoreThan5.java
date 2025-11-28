7ed5caf22706c12e7949af8d443fe9befca4352d
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-28 14:43:13 +0100

    feat: created FilterByOLengthGreaterThan5

--- src/main/java/exercise02/FilterByLetterOAndMoreThan5.java
+++ src/main/java/exercise02/FilterByLetterOAndMoreThan5.java
@@ -3,8 +3,8 @@ package exercise02;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class FilterByLetterOAndLessThan5 {
-    public List<String> filter(List<String> list) {
+public class FilterByLetterOAndMoreThan5 {
+    public static List<String> filter(List<String> list) {
         return list.stream()
                 .filter(w -> w.contains("o"))
                 .filter(x -> x.length() > 5)
---
+++ src/test/java/FilterByOLengthGreaterThan5.java
@@ -0,0 +1,21 @@
+import exercise02.FilterByLetterOAndMoreThan5;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FilterByOLengthGreaterThan5 {
+    @Test
+    void shouldFilterByOAndLengthGreaterThan5() {
+        List<String> words = Arrays.asList(
+                "car", "motorcycle", "computer", "operation", "dog", "language"
+        );
+        List<String> expected = Arrays.asList("motorcycle", "computer", "operation", "language");
+        List<String> actual = FilterByLetterOAndMoreThan5.filter(words);
+
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected) && expected.containsAll(actual));
+}
